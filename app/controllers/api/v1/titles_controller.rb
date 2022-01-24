7352a6943b8d03f676e2328de26cbe3371ce15e3
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2022-01-24 18:54:24 -0300

    started listed_in feature

--- app/controllers/api/v1/titles_controller.rb
+++ app/controllers/api/v1/titles_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::TitlesController < Api::V1::BaseController
   def index
     if params[:release_year].present?
       @titles = Title.where(release_year: params[:release_year])
+    elsif params[:listed_in].present?
+      @titles = Title.where(listed_in: params[:listed_in])
     else
       @titles = Title.all
     end
