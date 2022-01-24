b560b5ebca6bfee5e4a093abde739fd1fd2bac1b
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2022-01-24 19:11:58 -0300

    fixed querying inside array column

--- app/controllers/api/v1/titles_controller.rb
+++ app/controllers/api/v1/titles_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::TitlesController < Api::V1::BaseController
     if params[:release_year].present?
       @titles = Title.where(release_year: params[:release_year])
     elsif params[:listed_in].present?
-      @titles = Title.where(listed_in: params[:listed_in])
+      @titles = Title.where(":listed_in = ANY(listed_in)", listed_in: params[:listed_in])
     elsif params[:country].present?
-      @titles = Title.where(listed_in: params[:country])
+      @titles = Title.where(":country = ANY(country)", country: params[:country])
     else
       @titles = Title.all
     end
